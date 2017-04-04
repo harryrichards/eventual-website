4a55c47f77e42e80f99652329cf6e6ca9079f13b
harryrichards <hello@harryrichards.me>
2017-04-04 15:40:40 +0100

    added gradients

--- js/index.js
+++ js/index.js
@@ -1,48 +1,40 @@
-
 // when the user clicks sign up
 // scroll to the form 
-
 $('.sign-up').click(function() {
-    $('html,body').animate({
-        scrollTop: $('.enquiry').offset().top},
-        1500);
-});
+$('html,body').animate({scrollTop: $('.enquiry').offset().top}, 1500);});
+
+
+
+// when the user is in view of the screenshots section
+// play the video
+$('.screenshots').each(function(){
+    if ($('video').is(":in-viewport")) {
+        $('video')[0].play();
+    } 
+    else {
+        $('video')[0].pause();
+    }
+})
 
 
 window.sr = ScrollReveal();
+// when scrolling over the screenshots section
+// reveal the iphone
+sr.reveal('.phone-box', {origin: 'top', duration: 2500, delay:150});
 
-// when scrolling to the numbers section
+// when scrolling over the numbers section
 // reveal the icons from the top down
 sr.reveal('.numbers-icon', {origin: 'top', duration: 2000, delay:150});
-// when scrolling to the benefits section
+
+// when scrolling over the benefits section
 // reveal the benefits from the bottom up
 sr.reveal('.benefit-block', {origin: 'bottom', duration: 2000, delay: 150} );
-// when scrolling to the enquiry section
+
+// when scrolling over the enquiry section
 // reveal the form from the bottom up
 sr.reveal('.form-style', {origin: 'bottom', duration: 2500, delay: 150} );
 
 
 
-
-
-
-
-
-
-
-// Screenshots
-
-	// When the user scrolls to the screenshot section
-		// play first video
-
-	// when the user clicks on the number 2
-		// reveal 2nd video and 
-		// hide previous video
-		// and play the video
-
-	// when the user clicks on the number 3
-		// reveal the 3rd video
-		// hide previous video
-		// and play the new video
 
 
